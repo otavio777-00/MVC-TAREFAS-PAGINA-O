cadd6492457ac990adfda7c88f3507903fc24e7a
otavio777-00 <alvarenga.otavio08@gmail.com>
2026-05-21 10:20:17 +0000

    commit feito

---
+++ mvc-tarefas-paginacao/app/models/tarefasModel.js
@@ -0,0 +1,94 @@
+//requisição do pool de conexões
+const pool = require("../../config/pool_conexoes");
+//objeto com funções de acesso ao SGBD
+const tarefasModel = {
+    //funções para usar o SQL
+    findAll: async (offset = null, qtde = null) => {
+        try {
+            if (offset == null && qtde == null) {
+                var [linhas] = await pool.query("select * from tarefas where status_tarefa = 1");
+            } else {
+                var [linhas] = await pool.query(
+                    "select * from tarefas where status_tarefa = 1 " +
+                    " limit ? , ?", [offset, qtde]);
+            }
+
+            return linhas;
+        } catch (erro) {
+            return erro;
+        }
+    },
+
+    findById: async (id) => {
+        try {
+            const [linhas] = await pool.query(
+                "select * from tarefas where status_tarefa = 1 and id_tarefa = ? ",
+                [id]
+            );
+            return linhas;
+        } catch (erro) {
+            return erro;
+        }
+    },
+
+    create: async (dados) => {
+        /*
+            formato json
+            {
+                nome:"nome da tarefa",
+                prazo:"data mysql",
+                situacao:"cod situação",
+            }
+        */
+        try {
+            const [result] = await pool.query(
+                "insert into tarefas(`nome_tarefa`,`prazo_tarefa`,`situacao_tarefa`) "
+                + "values(?,?,?)",
+                [dados.nome, dados.prazo, dados.situacao]
+            )
+            return result;
+        } catch (erro) {
+            return erro;
+        }
+    },
+
+    // UPDATE - Todos os campos
+    update: async (dados) => {
+        /*
+            formato json
+            {
+                id: 9 
+                nome:"nome da tarefa",
+                prazo:"data mysql",
+                situacao:"cod situação",
+            }
+        */
+        try {
+            const [result] = await pool.query(
+                "update  tarefas set `nome_tarefa`= ? ,`prazo_tarefa`= ?,`situacao_tarefa`= ? "
+                + " where id_tarefa = ?",
+                [dados.nome, dados.prazo, dados.situacao, dados.id]
+            )
+            return result;
+        } catch (erro) {
+            return erro;
+        }
+    },
+
+
+    totRegistros: async () => {
+        try {
+            const [result] = await pool.query("SELECT count(*) as total FROM `lista-tarefas`.tarefas;");
+            return result[0].total;
+        } catch (erro) {
+            return erro;
+        }
+    }
+
+    // DELETE - físico
+    // DELETE - lógico
+
+}
+
+//exportar o objeto como um módulo do JS
+module.exports = { tarefasModel } 
---
+++ mvc-tarefas-paginacao/app/routes/router.js
@@ -0,0 +1,122 @@
+var express = require("express");
+var router = express.Router();
+const { tarefasModel } = require("../models/tarefasModel"); //usar sempre o {}
+const moment = require("moment");
+moment.locale('pt-br');
+
+router.get("/", async function (req, res) {
+    res.locals.moment = moment;
+    //recuperar a página caso não exista definir como página 1
+    let pagina = req.query.pagina == undefined ? 1 : req.query.pagina;
+    // página -> offset 
+    // qtde por página -> 
+    // pagina 1 -> ofsset -> 0 ( pagina -1 * 5)
+    // pagina 2 -> ofsset -> 5 ( pagina -1 * 5)
+    // pagina 3 -> ofsset -> 10 ( pagina -1 * 5)
+    const qtde = 5;
+    let offset = (pagina-1) * qtde;
+    let total = Math.ceil( await tarefasModel.totRegistros() / qtde);
+
+    if(total > 1){
+        //paginação 
+        var paginador = {paginaAtual:pagina,totalPaginas:total};
+        
+    }else{
+        // apenas 1 página de resultados
+        var paginador = null;
+    }
+    
+    try {
+        const result = await tarefasModel.findAll(offset,qtde);
+        console.log(result)
+        res.render("pages/index",
+             { listaTarefas: result, notificador:paginador })
+    } catch (erro) {
+        console.log(erro);
+    }
+});
+
+
+router.get("/nova-tarefa", (req, res) => {
+    res.locals.moment = moment;
+    res.render("pages/cadastro",
+        {
+            tituloPagina: "Cadastro de Tarefas", tituloAba: "Cadastro",
+            tarefa: {
+                id_tarefa: 0, nome_tarefa: "",
+                prazo_tarefa: "", situacao_tarefa: 1
+            }
+        });
+});
+
+router.post("/manter-tarefa", async (req, res) => {
+    const objDados = {
+        id : req.body.id,
+        nome: req.body.nome,
+        prazo: req.body.prazo,
+        situacao: req.body.situacao
+    }
+
+    try {
+        if(objDados.id == 0){
+            const result = await tarefasModel.create(objDados);  
+        }else{
+            const result = await tarefasModel.update(objDados);  
+        }
+        
+        res.redirect("/");
+    } catch (erro) {
+        console.log(erro);
+    }
+})
+
+
+router.get("/editar", async (req, res) => {
+    res.locals.moment = moment;
+    //recuperando a querystring
+    const id = req.query.id;
+    try {
+        const result = await tarefasModel.findById(id);
+        res.render("pages/cadastro",
+            {
+                tituloPagina: "Alterar Tarefa", tituloAba: "Edição de Tarefa",
+                tarefa: result[0]
+            });
+    } catch (erro) {
+        console.log(erro)
+    }
+
+});
+
+
+router.get("/teste-insert", async (req, res) => {
+
+    const objDados = {
+        nome: "limpar gabinete PC",
+        prazo: "2026-03-23"
+    }
+    try {
+        const result = await tarefasModel.create(objDados);
+        res.send(result);
+    } catch (erro) {
+        console.log(erro);
+    }
+});
+
+//exclusão física - hard delete
+router.get("/teste-delete", async (req, res) => {
+
+
+
+})
+
+//exclusão lógica - soft delete
+router.get("/teste-delete-logico", async (req, res) => {
+
+
+})
+
+
+
+
+module.exports = router;
